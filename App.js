719883e16162eaa678ffd86bf7debe7067461bc1
paulakedouk <paulakedouk@gmail.com>
2017-11-09 15:27:36 -0800

    Add Redux

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
 import AddEntry from './components/AddEntry';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log('Before');
-    debugger;
-    console.log('After');
-  }
-
   render() {
     return (
-      <View>
-        <AddEntry />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
     );
   }
 }
